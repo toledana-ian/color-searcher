7d9c0d74b86303f3e854083285f852dff49ad55c
Christian Toledana <toledana.ian@gmail.com>
2022-08-18 21:23:24 +0800

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,132 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from "axios";
+import 'react-notifications-component/dist/theme.css'
+import {ReactNotifications, Store} from 'react-notifications-component'
+
+interface ColorModel{
+    name: string,
+    hex: string,
+    luminance: number
+}
+
+const CardComponent = (props: ColorModel) => {
+    const textColor = props.luminance<65?'white':'black';
+
+    return (
+        <>
+            <div
+                className="block p-6 max-w-sm rounded-lg border border-black shadow-md"
+                style={{
+                    background:props.hex,
+                    color: textColor
+                }}
+            >
+                <div
+                    className={'font-bold text-center cursor-pointer'}
+                    onClick={()=>{
+                        navigator.clipboard.writeText(props.name);
+                        Store.removeAllNotifications();
+                        Store.addNotification({
+                            title: "Success!",
+                            message: 'Copied '+props.name+' to clipboard',
+                            type: "success",
+                            insert: "top",
+                            container: "top-right",
+                            animationIn: ["animate__animated", "animate__fadeIn"],
+                            animationOut: ["animate__animated", "animate__fadeOut"],
+                            dismiss: {
+                                duration: 5000,
+                                onScreen: true
+                            }
+                        });
+                    }}
+                >
+                    {props.name}
+                </div>
+                <div
+                    className={'font-bold text-center cursor-pointer'}
+                    onClick={()=>{
+                        navigator.clipboard.writeText(props.hex);
+                        Store.removeAllNotifications();
+                        Store.addNotification({
+                            title: "Success!",
+                            message: 'Copied '+props.hex+' to clipboard',
+                            type: "success",
+                            insert: "top",
+                            container: "top-right",
+                            animationIn: ["animate__animated", "animate__fadeIn"],
+                            animationOut: ["animate__animated", "animate__fadeOut"],
+                            dismiss: {
+                                duration: 5000,
+                                onScreen: true
+                            }
+                        });
+                    }}
+                >
+                    {props.hex}
+                </div>
+            </div>
+        </>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [colors, setColors] = useState<ColorModel[]>([]);
+
+    const onChangeSearch = (event: React.FormEvent<HTMLInputElement>) => {
+        const searchValue = event.currentTarget.value;
+
+        axios
+            .get('https://api.color.pizza/v1/names/'+searchValue)
+            .then((response)=>{
+                setColors(
+                    response.data.colors
+                );
+            })
+        ;
+    }
+
+    return (
+        <div className="App">
+            <ReactNotifications/>
+            <div className={'container mx-auto p-4 mt-4'}>
+                <form className="flex items-center max-w-md mx-auto">
+                    <label htmlFor="simple-search" className="sr-only">Search</label>
+                    <div className="relative w-full">
+                        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd"
+                                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        </div>
+                        <input type="text" id="simple-search"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                               placeholder="Search" onChange={onChangeSearch} required/>
+                    </div>
+                </form>
+
+                <div className={'flex flex-wrap justify-center gap-4 mt-2 p-2'}>
+                    {
+                        colors.map((color) => {
+                            return (
+                                <>
+                                    <CardComponent
+                                        name={color.name}
+                                        hex={color.hex}
+                                        luminance={color.luminance}
+                                    />
+                                </>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
