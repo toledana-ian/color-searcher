003cf1ed2b63f25209dd7e02f8f48459651800aa
Christian Toledana <toledana.ian@gmail.com>
2022-08-20 04:37:03 +0800

    Added shuffle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import 'react-notifications-component/dist/theme.css'
-import {ReactNotifications, Store} from 'react-notifications-component'
+import 'react-notifications-component/dist/theme.css';
+import {ReactNotifications, Store} from 'react-notifications-component';
+import 'flowbite-react';
+import { Tooltip } from 'flowbite-react';
+
 
 interface ColorModel{
     name: string,
@@ -50,8 +53,6 @@ const CardComponent = (props: ColorModel) => {
 }
 
 function App() {
-
-
     const [colors, setColors] = useState<ColorModel[]>([]);
 
     const onChangeSearch = (event: React.FormEvent<HTMLInputElement>) => {
@@ -65,6 +66,29 @@ function App() {
                 );
             })
         ;
+    }
+
+    const onClickShuffle = (_: React.FormEvent<HTMLButtonElement>) => {
+        const shuffle = (array:any[])=>{
+            let output = JSON.parse(JSON.stringify(array));
+            let currentIndex = output.length,  randomIndex;
+
+            // While there remain elements to shuffle.
+            while (currentIndex !== 0) {
+
+                // Pick a remaining element.
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
+
+                // And swap it with the current element.
+                [output[currentIndex], output[randomIndex]] = [
+                    output[randomIndex], output[currentIndex]];
+            }
+
+            return output;
+        }
+
+        setColors(shuffle(colors));
     }
 
     return (
@@ -86,20 +110,27 @@ function App() {
                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                placeholder="Search" onChange={onChangeSearch} required/>
                     </div>
-                    <button
-                        data-tooltip-target="tooltip-light"
-                        data-tooltip-style="light"
-                        type="button"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+
+                    <Tooltip
+                        content="Shuffle"
                     >
-                        <svg xmlns="http://www.w3.org/2000/svg" className={'w-5 h-5'} fill="currentColor"
-                             viewBox="0 0 16 16">
-                            <path fillRule="evenodd"
-                                  d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"/>
-                            <path
-                                d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z"/>
-                        </svg>
-                    </button>
+                        <button
+                            data-tooltip-target="tooltip-light"
+                            data-tooltip-style="light"
+                            type="button"
+                            onClick={onClickShuffle}
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className={'w-5 h-5'} fill="currentColor"
+                                 viewBox="0 0 16 16">
+                                <path fillRule="evenodd"
+                                      d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"/>
+                                <path
+                                    d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z"/>
+                            </svg>
+                        </button>
+                    </Tooltip>
+
                 </form>
 
                 <div className={'flex flex-wrap justify-center gap-4 mt-2 p-2'}>
@@ -117,12 +148,6 @@ function App() {
                         })
                     }
                 </div>
-            </div>
-
-            <div id="tooltip-light" role="tooltip"
-                 className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 shadow-sm opacity-0 tooltip">
-                Tooltip content
-                <div className="tooltip-arrow" data-popper-arrow/>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,tsx}'],
+  content: [
+      './src/**/*.{html,tsx}',
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
